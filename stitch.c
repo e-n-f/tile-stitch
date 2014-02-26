4ec044fe873407191f3cf717d6b9490f6233ddba
Eric Fischer <enf@pobox.com>
2014-02-25 17:09:29 -0800

    Decode bounding box into tile coordinates

--- stitch.c
+++ stitch.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <unistd.h>
 #include <curl/curl.h>
 
 void usage(char **argv) {
 	fprintf(stderr, "Usage: %s [-o outfile] minlat minlon maxlat maxlon zoom http://whatever/{z}/{x}/{y}.png\n", argv[0]);
+}
+
+// http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+void latlon2tile(double lat, double lon, int zoom, unsigned int *x, unsigned int *y) {
+	double lat_rad = lat * M_PI / 180;
+	unsigned long long n = 1LL << zoom;
+
+	*x = n * ((lon + 180) / 360);
+	*y = n * (1 - (log(tan(lat_rad) + 1/cos(lat_rad)) / M_PI)) / 2;
 }
 
 int main(int argc, char **argv) {
@@ -25,4 +35,23 @@ int main(int argc, char **argv) {
 			exit(EXIT_FAILURE);
 		}
 	}
+
+	if (argc - optind != 6) {
+		usage(argv);
+		exit(EXIT_FAILURE);
+	}
+
+	double minlat = atof(argv[optind]);
+	double minlon = atof(argv[optind + 1]);
+	double maxlat = atof(argv[optind + 2]);
+	double maxlon = atof(argv[optind + 3]);
+	int zoom = atoi(argv[optind + 4]);
+
+	unsigned int x1, y1, x2, y2;
+	latlon2tile(maxlat, minlon, 32, &x1, &y1);
+	latlon2tile(minlat, maxlon, 32, &x2, &y2);
+
+	printf("at zoom level %d, that's %d/%d to %d/%d\n", zoom,
+		x1 >> (32 - zoom), y1 >> (32 - zoom),
+		x2 >> (32 - zoom), y2 >> (32 - zoom));
 }
