7c48e6c0f666e9394f0ae3ad823ee5980ad4844e
Eric Fischer <enf@pobox.com>
2019-03-25 15:27:19 -0700

    Make centering actually work (again?)

--- stitch.c
+++ stitch.c
@@ -183,7 +183,7 @@ int main(int argc, char **argv) {
 	int centered = 0;
 	int outfmt = OUTFMT_PNG;
 	unsigned int writeworldfile = FALSE;
-	while ((i = getopt(argc, argv, "o:t:c:f:w")) != -1) {
+	while ((i = getopt(argc, argv, "o:t:cf:w")) != -1) {
 		switch (i) {
 		case 'o':
 			outfile = optarg;
